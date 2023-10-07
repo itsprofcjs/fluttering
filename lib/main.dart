32f62b84e4146ba25d8a4109e2f7b3ac7f9531f3
Rupesh Kumar Singh <itsprofcjs@gmail.com>
2023-10-07 15:19:50 +0530

    feat: rolling the dice :)

--- lib/main.dart
+++ lib/main.dart
@@ -13,73 +13,56 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/space.webp'),
-              ),
-              Text(
-                'CJS !',
-                style: GoogleFonts.pacifico(
-                  textStyle: TextStyle(
-                    color: Colors.cyanAccent,
-                    fontSize: 40,
-                    letterSpacing: 2,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Fluttering Flutter Development'.toUpperCase(),
-                style: GoogleFonts.sourceSans3(
-                  textStyle: TextStyle(
-                    color: Colors.blue.shade100,
-                    fontSize: 25,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-                width: 300,
-                child: Divider(
-                  color: Colors.blue.shade100,
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(
-                    Icons.add,
-                    color: Colors.indigo,
-                  ),
-                  title: Text(
-                    'add me to ',
-                    style: TextStyle(color: Colors.indigo),
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(
-                    Icons.mail,
-                    color: Colors.indigo,
-                  ),
-                  title: Text(
-                    'ascljanflnls@cjs.com',
-                    style: TextStyle(color: Colors.indigo),
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 25, horizontal: 30),
-              )
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
+        appBar: AppBar(
+          title: Text('Dicing...'),
         ),
+        body: SafeArea(child: DicePage()),
       ),
     );
+  }
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Left dice rolled');
+              },
+              child: Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('Right dice rolled');
+              },
+              child: Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Image.asset('images/dice2.png'),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BackgroundContainer extends StatelessWidget {
+  const BackgroundContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
